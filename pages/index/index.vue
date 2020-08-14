37a1b47e64106abe973d50eba12ef5c225c49ad2
ANjh <anai261217@126.com>
2020-08-14 13:06:55 +0800

    my-app init

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="index">
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="my">
+		我的信息
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
